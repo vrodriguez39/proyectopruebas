d25d31cce4887fde163ad6573d874a219ded8dff
vrodriguez39 <victor.rodriguezhernandez@cesunbc.edu.mx>
2022-11-09 09:25:24 -0800

    main

--- test/auth.js
+++ test/auth.js
@@ -13,14 +13,75 @@ describe("Como administrador quiero hacer login:", function () {
     it("Autentificacion", async () => {
       const usuarios = {
         name: "Nuevo Usuario 11",
-        email: "vrodriguez33@gmail.com",
-        password: "admin",
+        email: "vrodriguez38@gmail.com",
+      password: "admin",
         rol: "admin",
         sucursal: "Tecate",
-      };
+      }
       const response = await axios.post(API_URL + "/register", usuarios);
       expect(response.status).to.be.equal(201);
       expect(response.data).to.be.an("object");
       //console.log(response.data); muestra en consola
     });
 })
+
+ 
+
+/* const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const {Todo} = require('../models/todo');
+const {User} = require('../models/user1');
+
+const userOneID = new ObjectID();
+const userTwoID = new ObjectID();
+const users = [{
+  _id: userOneID,
+  email: "userone@gmail.com",
+  password: "useronepassword",
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneID, access: 'auth'}, process.env.JWT_SECRET).toString()
+  }]
+}, {
+  _id: userTwoID,
+  email: "usertwo@gmail.com",
+  password: "usertwopassword",
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoID, access: 'auth'}, process.env.JWT_SECRET).toString()
+  }]
+}]
+
+const todos = [{
+  _id: new ObjectID(),
+  text: "First test todo",
+  _creator: userOneID
+}, {
+  _id: new ObjectID(),
+  text: "Second test todo",
+  _creator: userTwoID
+}];
+
+var populateTodos = (done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+};
+
+var populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo])
+  }).then(() => done());
+};
+
+module.exports = {
+  todos,
+  populateTodos,
+  users,
+  populateUsers
+}
+ */
--- test/useradmin.js
+++ test/useradmin.js
@@ -9,11 +9,11 @@ before(async () => {
   newuser = await createUser();
 });
 
-describe("Como administrador quiero administrar Usuarios:", function () {
+/* describe("Como administrador quiero administrar Usuarios:", function () {
   it("Agregar usuario", async () => {
     const usuarios = {
       name: "Nuevo Usuario 3",
-      email: "nuevo1@gmail.com",
+      email: "nuevo35@gmail.com",
       password: "admin",
       rol: "admin",
       sucursal: "Tecate",
@@ -59,70 +59,187 @@ describe("Como administrador quiero administrar Usuarios:", function () {
     expect(response.status).to.be.equal(200);
     expect(response.data).to.be.an("object");
   });
+}); */
 
+////////////MOSTRADOS DE OTRA FORMA///////////////////////////////////////////////
 
- ////////////MOSTRADOS DE OTRA FORMA///////////////////////////////////////////////
+describe("Como administrador quiero agregar usuario:", function () {
+  it("Usuario agregado", async () => {
+    const usuarios = {
+      name: "Nuevo Usuario 1",
+      email: "nuevo11@gmail.com",
+      password: "admin",
+      rol: "admin",
+      sucursal: "Tecate",
+    };
+    const response = await axios.post(API_URL + "/postuserinfo", usuarios);
 
- 
-  /* describe("Como administrador quiero agregar usuario:", function () {
-    it("Usuario agregado", async () => {
+    expect(response.status).to.be.equal(201);
+    expect(response.data).to.be.an("object");
+    //console.log(response.data); muestra en consola
+  });
+
+  ///////////////////Error por falta de Usuario//////////////////////////////////
+
+  it("Falta el campo de usuario", async () => {
+    try {
       const usuarios = {
-        name: "Nuevo Usuario 1",
-        email: "nuevo1@gmail.com",
+        //name: "Nuevo Usuario 1",
+        email: "nuevo11@gmail.com",
         password: "admin",
         rol: "admin",
         sucursal: "Tecate",
       };
       const response = await axios.post(API_URL + "/postuserinfo", usuarios);
-      expect(response.status).to.be.equal(201);
+      expect(error.response.status).not.to.be.equal(201);
       expect(response.data).to.be.an("object");
-      //console.log(response.data); muestra en consola
-    });
+
+    } catch (error) {
+      if (error.response) {
+        expect(error.response.status).not.to.be.equal(201);
+      } else {
+        throw error;
+      }
+    }
+  });
+///////////////////Error por falta de Email//////////////////////////////////
+it("Falta el campo de email", async () => {
+  try {
+    const usuarios = {
+      name: "Nuevo Usuario 1",
+      //email: "nuevo11@gmail.com",
+      password: "admin",
+      rol: "admin",
+      sucursal: "Tecate",
+    };
+    const response = await axios.post(API_URL + "/postuserinfo", usuarios);
+    expect(error.response.status).not.to.be.equal(201);
+    expect(response.data).to.be.an("object");
+
+  } catch (error) {
+    if (error.response) {
+      expect(error.response.status).not.to.be.equal(201);
+    } else {
+      throw error;
+    }
+  }
+});
+///////////////////Error por falta de Password//////////////////////////////////
+it("Falta el campo de Password", async () => {
+  try {
+    const usuarios = {
+      name: "Nuevo Usuario 1",
+      email: "nuevo11@gmail.com",
+      //password: "admin",
+      rol: "admin",
+      sucursal: "Tecate",
+    };
+    const response = await axios.post(API_URL + "/postuserinfo", usuarios);
+    expect(error.response.status).not.to.be.equal(201);
+    expect(response.data).to.be.an("object");
+
+  } catch (error) {
+    if (error.response) {
+      expect(error.response.status).not.to.be.equal(201);
+    } else {
+      throw error;
+    }
+  }
+});
+
+///////////////////Error por correo Existente//////////////////////////////////
+/* it("Error de Correo ya existente", async () => {
+   // Validando
+   const errors = validationResult(req);
+   if (!errors.isEmpty())
+     return res.status(422).json(validation(errors.array()));
+ 
+   const { name, email, password,rol,sucursal } = req.body;
+ 
+   try {
+     let user = await User.findOne({ email: email.toLowerCase() });
+ 
+     // Encontrar por email
+     if (user)
+       return res
+         .status(422)
+         .json(validation({ msg: "Email ya registrado" }));
+ 
+     let newUser = new User({
+       name,
+       email: email.toLowerCase().replace(/\s+/, ""),
+       password,
+       rol,
+       sucursal
+     });
+ 
+   
+   } catch (err) {
+     console.error(err.message);
+     res.status(500).json(error("Server error", res.statusCode));
+   }
+ }); */
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+describe("Como administrador quiero ver todos los usuarios:", function () {
+  it("Usuarios mostrados", async () => {
+    const response = await axios.get(API_URL + "/listadeusuarios");
+    //console.log(response.data); muestra todos los usuarios en la consola
+    expect(response.status).to.be.equal(200);
+    expect(response.data).to.be.an("Array");
+  });
+});
+
+
+
+
+describe("Como administrador quiero ver detalle de un Usuario:", function () {
+  it("Detalles mostrados", async () => {
+    //console.log(newuser)
+
+    const response = await axios.get(API_URL + "/usuario/" + newuser._id);
+
+    expect(response.status).to.be.equal(200);
+    //expect(response.data).to.be.an("object");
   });
 
-  describe("Como administrador quiero ver todos los usuarios:", function () {
-    it("Usuarios mostrados", async () => {
-      const response = await axios.get(API_URL + "/listadeusuarios");
-      //console.log(response.data); muestra todos los usuarios en la consola
-      expect(response.status).to.be.equal(200);
-      expect(response.data).to.be.an("Array");
-    });
-  });
-
-  describe("Como administrador quiero ver detalle de un Usuario:", function () {
-    it("Detalles mostrados", async () => {
-      //console.log(newuser)
-
+  describe("Como administrador quiero editar un Usuario:", function () {
+    it("Editar Usuario", async () => {
       const response = await axios.get(API_URL + "/usuario/" + newuser._id);
 
+      expect(response.data.email).to.be.equals("nuevo@gmail.com");
+
+      response.data.should.have.property("name");
+      response.data.should.have.property("email");
+      response.data.should.have.property("password");
+      response.data.should.have.property("rol");
+      response.data.should.have.property("sucursal");
+
       expect(response.status).to.be.equal(200);
-      //expect(response.data).to.be.an("object");
+      expect(response.data).to.be.an("object");
     });
-
-    describe("Como administrador quiero editar un Usuario:", function () {
-      it("Editar Usuario", async () => {
-        const response = await axios.get(API_URL + "/usuario/" + newuser._id);
-
-        expect(response.data.email).to.be.equals("nuevo@gmail.com");
-
-        response.data.should.have.property("name");
-        response.data.should.have.property("email");
-        response.data.should.have.property("password");
-        response.data.should.have.property("rol");
-        response.data.should.have.property("sucursal");
-
-        expect(response.status).to.be.equal(200);
-        expect(response.data).to.be.an("object");
-      });
+  });
+  describe("Como administrador quiero eliminar un Usuario:", function () {
+    it("Usuario Eliminado", async () => {
+      const response = await axios.delete(
+        API_URL + "/deleteuser/" + newuser._id
+      );
+      expect(response.status).to.be.equal(200);
+      expect(response.data).to.be.an("object");
     });
-    describe("Como administrador quiero eliminar un Usuario:", function () {
-      it("Usuario Eliminado", async () => {
-        const response = await axios.delete(
-          API_URL + "/deleteuser/" + newuser._id
-        );
-        expect(response.status).to.be.equal(200);
-        expect(response.data).to.be.an("object");
-      });
-    });
-  }); */
+  });
 });
